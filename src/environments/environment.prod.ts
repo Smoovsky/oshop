34de279fc159343528e3632c39feceb8d5adb0f4
smokovsky <chenshang920@hotmail.com>
2018-08-03 00:39:11 +1000

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD07Uc8dzKqECE3HTKyBXaXnxDdK8OHMiw',
+    authDomain: 'oshop-5ef3f.firebaseapp.com',
+    databaseURL: 'https://oshop-5ef3f.firebaseio.com',
+    projectId: 'oshop-5ef3f',
+    storageBucket: 'oshop-5ef3f.appspot.com',
+    messagingSenderId: '547733351337',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD07Uc8dzKqECE3HTKyBXaXnxDdK8OHMiw',
+    authDomain: 'oshop-5ef3f.firebaseapp.com',
+    databaseURL: 'https://oshop-5ef3f.firebaseio.com',
+    projectId: 'oshop-5ef3f',
+    storageBucket: 'oshop-5ef3f.appspot.com',
+    messagingSenderId: '547733351337',
+  }
 };
 
 /*
