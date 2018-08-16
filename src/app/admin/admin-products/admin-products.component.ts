2e08f6bfd0e0ec9d5fcf99e3c34106dbab5ffefe
smokovsky <chenshang920@hotmail.com>
2018-08-16 22:28:54 +1000

    add filtering for admin product list

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../../../../models/product';
 import { Observable, Subscription } from 'rxjs';
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
@@ -8,21 +9,21 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products;
+  products: Product[];
   subscription: Subscription;
-  filteredProducts;
+  filteredProducts: Product[];
 
 
   constructor(private productService: ProductService) {
-    this.subscription = productService.getAll().subscribe( p => this.filteredProducts = this.products = p);
+    this.subscription = productService.getAll<Product>().subscribe( p => this.filteredProducts = this.products = p);
    }
 
-   filter(query){
-     this.filteredProducts = (query) ? 
-       this.products$.filter( p => p.title.search)
+   filter(query) {
+     this.filteredProducts = (query) ?
+       this.products.filter( p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
    }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '../../node_modules/angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,14 @@ export class ProductService {
     return this.db.list('/products').push(product);
   }
 
-  getAll() {
-    return this.db.list('/products').snapshotChanges().pipe(
+  getAll<T>() {
+    return this.db.list<T>('/products').snapshotChanges().pipe(
       map(a =>
-        a.map( p =>
-          ({
-            key: p.key,
-            ...p.payload.val()
-          })
+        a.map( p => {
+            const value = <any>Object.assign({}, p.payload.val());
+            value.key = p.key;
+            return <T>value;
+          }
       ))
     );
   }
