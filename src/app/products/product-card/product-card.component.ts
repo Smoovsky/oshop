df35508937462fb724c12fd2d42d81861d55a6ae
smokovsky <chenshang920@hotmail.com>
2018-08-20 13:01:01 +1000

    finishing cart servie

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -14,13 +14,6 @@ export class ProductCardComponent {
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   addToCart(product: Product) {
-    let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      this.shoppingCartService.create().then(result => {
-        localStorage.setItem('cartId', result.key);
-      });
-    } else {
-      
-    }
+    this.shoppingCartService.addToCart(product);
   }
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,3 +1,6 @@
+import { Product } from 'models/product';
+import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
@@ -8,9 +11,43 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create() {
+
+  private create() {
     return this.db.list('/shopping-carts').push({
       dateCreate: new Date().getTime()
     });
   }
+
+  private getCart(cartId) {
+    return this.db.list('/shopping-carts/' + cartId);
+  }
+
+  private async getOrCreateCartId() {
+    const cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+      const result = await this.create();
+      localStorage.setItem('cartId', result.key);
+      return result.key;
+    }
+    return cartId;
+  }
+
+  private getItem(cartId, productId) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+
+  async addToCart(product: Product) {
+    const cartId = await this.getOrCreateCartId();
+
+    const item$F = this.getItem(cartId, product.key);
+    const item$ = item$F.valueChanges();
+    item$.pipe(take(1)).subscribe( item => {
+        item = item ? item : {};
+        item$F.update({
+        quantity: ((<any>item).quantity || 0) + 1,
+        product: product
+      });
+    }
+  );
+}
 }
