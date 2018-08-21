3f63c2cd3e2434488878b1bb1c806ea89d788750
smokovsky <chenshang920@hotmail.com>
2018-08-21 18:03:14 +1000

    work on checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
-
+export class CheckoutComponent implements OnInit, OnDestroy {
+  cart = null;
   shipping = {};
+  subscription: Subscription = null;
+  /**
+   *
+   */
+  constructor(private shoppingCartService: ShoppingCartService) {
+  }
 
   placeOrder() {
     console.log(this.shipping);
   }
 
-  ngOnInit() {
-
+  async ngOnInit() {
+    const cart$ = await this.shoppingCartService.getCart();
+    this.subscription = cart$.subscribe(cart => this.cart = cart);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
